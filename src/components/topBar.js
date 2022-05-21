98802fc42eb8d369e693f1c91c23f35a20124cdf
jaewonE <importjaewonE@gmail.com>
2022-05-21 22:34:52 +0900

    create 404 page

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const TopBar = () => {
   return (
-    <div className="relative top-0 h-14 flex px-12 border border-x-0 border-t-0">
+    <div className="w-full relative top-0 h-14 flex px-12 border border-x-0 border-t-0">
       <div className="w-1/2 h-full flex justify-start items-center">Logo</div>
       <div className="w-1/2 h-full flex justify-end items-center overflow-hidden">
         {/* <div className="h-1/2 w-5 bg-black rounded-full mr-4">i</div> */}
--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  return <div className="bluePrint">404 Not Found</div>;
+  return (
+    <div className="w-screen h-screen m-0 p-0 flex justify-center items-center bg-[#f1f1f4]">
+      <div className="w-5/6 sm:w-[600px] h-80 shadow-2xl flex flex-col justify-center items-center bg-white">
+        <span className="text-4xl font-semibold pb-3">404 Not Found</span>
+        <Link to={'/'} className="underline text-blue-500 text-lg">
+          <span>go to homepage</span>
+          <i className="pl-2 text-center text-base font-bold fa-solid fa-arrow-right"></i>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
